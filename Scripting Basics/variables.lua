50e9909850bf0e0432671919fda53674a3c78dd8
Sven <svenwu996@gmail.com>
2021-05-27 14:39:39 -0700

    local variables.

---
+++ Scripting Basics/variables.lua
@@ -0,0 +1,7 @@
+-- Create variables using the "local" keyword
+local dice1 = game.Workspace.GameParts.Dice.dice1
+wait(0.5) -- wait 0.5 seconds
+dice1.Anchored = false
+
+local text = "Greetings Master Sven."
+print(text)
