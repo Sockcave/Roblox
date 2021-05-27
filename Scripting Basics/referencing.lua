f5ad366bf655d056892a7a16502d0b66ab68956e
Sven <svenwu996@gmail.com>
2021-05-27 14:19:10 -0700

    Referencing and More Properties.

--- Scripting Basics/referencing.lua
+++ Scripting Basics/referencing.lua
@@ -2,6 +2,7 @@
 game.Workspace["Hi mom"].BrickColor = BrickColor.new("Really red")
 
 -- Part vs Property 
+
 -- We have a part named "Gravity" and a Workspace property called "Gravity"
 game.Workspace.Gravity -- the default Gravity call is the workspace property
 
@@ -9,3 +10,4 @@ game.Workspace.Gravity -- the default Gravity call is the workspace property
 game.Workspace.Gravity.BrickColor = BrickColor.new("Royal purple")
 --INSTEAD do this. This WILL WORK for the part.
 game.Workspace:FindFirstChild("Gravity").BrickColor = BrickColor.new("Royal purple")
+
